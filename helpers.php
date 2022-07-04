f483198d00745c388fed54c26319625b53054c11
Fernando Gutierrez <fernando@beeb.mx>
2022-07-04 11:14:56 -0500

    Remove illuminate/support dependency

---
+++ helpers.php
@@ -0,0 +1,30 @@
+<?php
+
+use Beebmx\Env;
+
+if (!function_exists('env')) {
+    /**
+     * Gets the value of an environment variable.
+     *
+     * @param  string  $key
+     * @param  mixed  $default
+     * @return mixed
+     */
+    function env($key, $default = null)
+    {
+        return Env::get($key, $default);
+    }
+}
+
+if (!function_exists('value')) {
+    /**
+     * Return the default value of the given value.
+     *
+     * @param  mixed  $value
+     * @return mixed
+     */
+    function value($value, ...$args)
+    {
+        return $value instanceof Closure ? $value(...$args) : $value;
+    }
+}
